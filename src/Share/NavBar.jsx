d823001a429bb846c8003d6093acd67bf9411340
Jubayer9 <121636287+Jubayer9@users.noreply.github.com>
2023-05-18 11:56:46 +0600

    Create a logging route

--- src/Share/NavBar.jsx
+++ src/Share/NavBar.jsx
@@ -2,36 +2,41 @@ import { Link } from "react-router-dom";
 
 const NavBar = () => {
     return (
-        <div>
-            <div className="navbar bg-base-100">
+        <div className="navbar bg-base-100">
   <div className="navbar-start">
     <div className="dropdown">
       <label tabIndex={0} className="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+        
       </label>
       <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/">Home</Link></li>
+        <li><Link>Home</Link></li>
+        <li><Link>All Toy </Link></li>
+        <li><Link>My Toy </Link></li>
+        <li><Link>Add Toy </Link></li>
+        <li><Link>Blog </Link></li>
         <li tabIndex={0}>
-          <Link className="justify-between">
+          <Link to='' className="justify-between">
             Parent
             
           </Link>
-          
+         
         </li>
-        <li><Link>Item 3</Link></li>
+        <li><Link>Item3</Link></li>
       </ul>
     </div>
-    <Link className="btn btn-ghost normal-case text-xl">daisyUI</Link>
+    <Link to='' className="btn btn-ghost normal-case text-xl">daisyUI</Link>
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
-      <li><Link to="/">Home</Link></li>
-      <li><Link to="/">Home</Link></li>
+      <li><Link>Home</Link></li>
+      <li><Link>All toy </Link></li>
+      <li><Link>My Toy </Link></li>
+      <li><Link>Add Toy </Link></li>
+      <li><Link>Blog </Link></li>
       <li tabIndex={0}>
         <Link>
           Parent
-         
+          
         </Link>
         
       </li>
@@ -39,10 +44,11 @@ const NavBar = () => {
     </ul>
   </div>
   <div className="navbar-end">
-    <Link className="btn">Get started</Link>
+    <Link to='' className="">Get started</Link>
   </div>
 </div>
-        </div>
+
+      
     );
 };
 
