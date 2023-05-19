faeed47d853d65d84e3fe446397047d9ed4f0d1f
Jubayer9 <121636287+Jubayer9@users.noreply.github.com>
2023-05-20 00:41:27 +0600

    Tap section add

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -3,6 +3,7 @@ import Footer from "../../../Share/Footer";
 import NavBar from "../../../Share/NavBar";
 import Header from "../Header/Header";
 import PhotoGallery from "../PhotoGallery/PhotoGallery";
+import Tab from "../Tab/TabBar";
 
 const Home = () => {
     return (
@@ -11,7 +12,7 @@ const Home = () => {
             <NavBar></NavBar>
             <Header></Header>
             <PhotoGallery></PhotoGallery>
-            
+            <Tab></Tab>
             <Footer></Footer>
 
         </div>
---
+++ src/Pages/Home/Tab/TabBar.jsx
@@ -0,0 +1,94 @@
+import { useEffect, useState } from 'react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+
+
+const TabBar = () => {
+  const [Categories, SetCategories] = useState([]);
+  useEffect(() => {
+
+  }, [])
+  return (
+    <Tabs>
+      <TabList>
+        <Tab style={{ marginLeft: "45%" }}>Title 1</Tab>
+        <Tab>Title 2</Tab>
+        <Tab>Title 3</Tab>
+      </TabList>
+      <TabPanel style={{ marginLeft: "25%" }}>
+        <div className='flex'>
+          <div className="card w-96 bg-base-100 shadow-xl">
+            <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+            <div className="card-body">
+              <h2 className="card-title">Shoes!</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary">Buy Now</button>
+              </div>
+            </div>
+          </div>
+          <div className="card w-96 bg-base-100 shadow-xl">
+            <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+            <div className="card-body">
+              <h2 className="card-title">Shoes!</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary">Buy Now</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </TabPanel>
+      <TabPanel style={{ marginLeft: "25%" }}>
+        <div className='flex'>
+          <div className="card w-96 bg-base-100 shadow-xl">
+            <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+            <div className="card-body">
+              <h2 className="card-title">Shoes!</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary">Buy Now</button>
+              </div>
+            </div>
+          </div>
+          <div className="card w-96 bg-base-100 shadow-xl">
+            <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+            <div className="card-body">
+              <h2 className="card-title">Shoes!</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary">Buy Now</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </TabPanel>
+      <TabPanel style={{ marginLeft: "25%" }}>
+        <div className='flex'>
+          <div className="card w-96 bg-base-100 shadow-xl">
+            <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+            <div className="card-body">
+              <h2 className="card-title">Shoes!</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary">Buy Now</button>
+              </div>
+            </div>
+          </div>
+          <div className="card w-96 bg-base-100 shadow-xl">
+            <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+            <div className="card-body">
+              <h2 className="card-title">Shoes!</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary">Buy Now</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </TabPanel>
+    </Tabs>
+  );
+};
+
+export default TabBar;
